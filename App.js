2ae5a1dd67c8aca0bc99f3a22e21d1ac6915fd16
Boriswaikin <plmnboris@gmail.com>
2023-01-24 18:24:46 -0800

    update image from url and require

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 
-import { StyleSheet, Text, View, Image, TextInput,Button} from 'react-native';
+import { Text, View, Image, TextInput,Button,StyleSheet} from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
 
 export default function App() {
   const name = "my awesome app"; 
   const [enteredText,sendChangedText] = useState();
+  const [modalvisible, setModalVisible] = useState(false);
 
-
+  function onCancel(status){
+    setModalVisible(status)
+  }
 
   return (
-    <View style={styles.container}>
+    <View style = {styles.container}>
       <Text></Text>
       <StatusBar style="auto" />
       <Header name = {name} />
-      <Input sendChangedText = {sendChangedText}/>
+      <Input 
+        modalvisible={modalvisible}
+        sendChangedText = {sendChangedText}
+        CancelPressed = {onCancel}/>
+      <Button 
+      title = "Add a Task"
+      onPress={()=>{
+        setModalVisible(true);
+      }}/>
       <Text> {enteredText} </Text>
     </View>
   );
--- components/Input.js
+++ components/Input.js
@@ -1,15 +1,18 @@
-import { View, TextInput,Button} from 'react-native'
+import { View, TextInput,Button, Modal, StyleSheet} from 'react-native'
 import { useState } from 'react';
 import React from 'react'
 
-const Input = ({sendChangedText}) => {
+const Input = ({sendChangedText,modalvisible,CancelPressed}) => {
     const [text,setText] = useState();
+   
     function changeTextHandler(changedText){
       setText(changedText);
       //sendChangedText(changedText);
     }
   return (
-    <View>
+    <Modal
+      visible = {modalvisible}>
+    <View >
       <TextInput 
       value={text}
       onChangeText={changeTextHandler}
@@ -19,8 +22,20 @@ const Input = ({sendChangedText}) => {
         sendChangedText(text);
         setText("");
       }}/>
+      <Button title="Cancel"
+      onPress={()=>{
+        CancelPressed(false);
+      }}/>
+      <Image
+        style={{ width: 150, height: 150 }}
+        source={{ uri: `https://cdn-icons-png.flaticon.com/512/2617/2617812.png` }}
+        />
+      <Image
+        style={{ width: 150, height: 150 }}
+        source={require('./image/image.png')}
+        />
     </View>
+    </Modal>
   )
 }
-
 export default Input
